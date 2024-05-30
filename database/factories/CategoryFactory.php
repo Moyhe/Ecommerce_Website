d0a3ed2972fd76795841a321131cf18ac5858ae1
Mohye <dragonsinmeliodas777@gmail.com>
2024-05-30 13:33:47 +0300

    configure deployment files

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
@@ -16,8 +17,13 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->unique()->word();
+
         return [
-            //
+
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'thumbnail' => fake()->imageUrl()
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +18,14 @@ class DatabaseSeeder extends Seeder
     {
 
 
-         \App\Models\Product::factory(10)->create();
+
+        //  \App\Models\Product::factory(10)->create();
+
+        Category::factory()
+            ->count(5)
+            ->hasAttached(Product::factory()->count(5))
+            ->create();
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
