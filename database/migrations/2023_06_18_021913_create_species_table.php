c73f5d85f38f780d4e6861b864747708b3fceabb
Mohye <dragonsinmeliodas777@gmail.com>
2024-05-30 20:33:58 +0300

    configure deployment files

--- database/migrations/2023_06_18_021913_create_species_table.php
+++ database/migrations/2023_06_18_021913_create_species_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('species', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->string('slug')->unique();
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('species');
     }
 };
