dc3e357bda5e0835f3873e380c71689b78afbcf0
Mohye <dragonsinmeliodas777@gmail.com>
2024-05-30 17:03:12 +0300

    configure deployment files

--- routes/web.php
+++ routes/web.php
@@ -37,23 +37,13 @@ Route::get('/shop', [ShopController::class, 'index'])->name('shop');
 Route::get('/shop/{product:slug}', [ShopController::class, 'show'])->name('shop.show');
 
 
-Route::get('/cart', [CartController::class, 'index'])->name('cart');
-Route::post('/cart/{product}', [CartController::class, 'store'])->name('cart.store');
-Route::patch('/cart/{product}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/{product}', [CartController::class, 'destroy'])->name('cart.destroy');
-Route::post('/cart/switchToSaveForLater/{product}', [CartController::class, 'switchToSaveForLater'])->name('cart.saveForLater');
 
-
-Route::delete('/saveForLater/{product}', [SaveForLaterController::class, 'destroy'])->name('saveForLater.destroy');
-Route::post('/saveForLater/switchToCart/{product}', [SaveForLaterController::class, 'switchToCart'])->name('saveForLater.switchToCart');
 
 
 Route::post('/coupon', [CouponController::class, 'store'])->name('coupon.store');
 Route::delete('/coupon', [CouponController::class, 'destroy'])->name('coupon.destroy');
 
-Route::get('/wihslist', [WishListController::class, 'index'])->name('wishlist');
-Route::post('/wihslist/{product}', [WishListController::class, 'store'])->name('wihslist.store');
-Route::delete('/wihslist/{product}', [WishListController::class, 'destroy'])->name('wihslist.destroy');
+
 
 
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
@@ -67,8 +57,8 @@ Route::get('/about-us', [AboutUsController::class, 'index'])->name('about-us');
 Route::get('/contact-us', [ContactUsController::class, 'index'])->name('contact-us');
 Route::post('/contact/submit', [ContactUsController::class, 'submit'])->name('contact');
 
-Route::get('empty', function(){
-     Cart::destroy();
+Route::get('empty', function () {
+    Cart::destroy();
 });
 
 Route::get('/dashboard', function () {
@@ -79,7 +69,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // wish list routes
+    Route::get('/wihslist', [WishListController::class, 'index'])->name('wishlist');
+    Route::post('/wihslist/{product}', [WishListController::class, 'store'])->name('wihslist.store');
+    Route::delete('/wihslist/{product}', [WishListController::class, 'destroy'])->name('wihslist.destroy');
+
+    // cart routes
+    Route::get('/cart', [CartController::class, 'index'])->name('cart');
+    Route::post('/cart/{product}', [CartController::class, 'store'])->name('cart.store');
+    Route::patch('/cart/{product}', [CartController::class, 'update'])->name('cart.update');
+    Route::delete('/cart/{product}', [CartController::class, 'destroy'])->name('cart.destroy');
+    Route::post('/cart/switchToSaveForLater/{product}', [CartController::class, 'switchToSaveForLater'])->name('cart.saveForLater');
+
+    // save for later routes
+    Route::delete('/saveForLater/{product}', [SaveForLaterController::class, 'destroy'])->name('saveForLater.destroy');
+    Route::post('/saveForLater/switchToCart/{product}', [SaveForLaterController::class, 'switchToCart'])->name('saveForLater.switchToCart');
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
